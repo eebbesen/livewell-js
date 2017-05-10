06671fab379cad5d9cf5481acd9b8c46fe0f0dfe
Eric Ebbesen <eebbesen@eebbesen-mbp.local>
2017-05-09 22:15:36 -0500

    Showing income limits

--- app.js
+++ app.js
@@ -1,66 +1,80 @@
-var express = require('express')
-var app = express();
+var express = require('express');
+var bodyParser = require('body-parser');
 
-var incomeLimitss = {
-  1: {
-    p30: 18050,
-    p50: 30050,
-    p60: 36060,
-    p80: 46000,
-    year: 2016
+var incomeLimits = {
+  '1': {
+    'p30': 18050,
+    'p50': 30050,
+    'p60': 36060,
+    'p80': 46000,
+    'year': 2016
   },
-  2: {
-    p30: 20060,
-    p50: 34350,
-    p60: 41220,
-    p80: 52600,
-    year: 2016
+  '2': {
+    'p30': 20060,
+    'p50': 34350,
+    'p60': 41220,
+    'p80': 52600,
+    'year': 2016
   },
-  3: {
-    p30: 23200,
-    p50: 38650,
-    p60: 46380,
-    p80: 59150,
-    year: 2016
+  '3': {
+    'p30': 23200,
+    'p50': 38650,
+    'p60': 46380,
+    'p80': 59150,
+    'year': 2016
   },
-  4: {
-    p30: 25750,
-    p50: 42900,
-    p60: 51480,
-    p80: 65700,
-    year: 2016
+  '4': {
+    'p30': 25750,
+    'p50': 42900,
+    'p60': 51480,
+    'p80': 65700,
+    'year': 2016
   },
-  5: {
-    p30: 27850,
-    p50: 46350,
-    p60: 55620,
-    p80: 71000,
-    year: 2016
+  '5': {
+    'p30': 27850,
+    'p50': 46350,
+    'p60': 55620,
+    'p80': 71000,
+    'year': 2016
   },
-  6: {
-    p30: 29900,
-    p50: 49800,
-    p60: 59760,
-    p80: 76250,
-    year: 2016
+  '6': {
+    'p30': 29900,
+    'p50': 49800,
+    'p60': 59760,
+    'p80': 76250,
+    'year': 2016
   },
-  7: {
-    p30: 31950,
-    p50: 53200,
-    p60: 63840,
-    p80: 81500,
-    year: 2016
+  '7': {
+    'p30': 31950,
+    'p50': 53200,
+    'p60': 63840,
+    'p80': 81500,
+    'year': 2016
   },
-  8: {
-    p30: 34000,
-    p50: 56650,
-    p60: 67980,
-    p80: 86750,
-    year: 2016
+  '8': {
+    'p30': 34000,
+    'p50': 56650,
+    'p60': 67980,
+    'p80': 86750,
+    'year': 2016
   }
 }
 
+
+var app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.listen(3000, function(){
   console.log('listening at port 3000');
-  console.log(incomeLimitss);
+  console.log(incomeLimits);
 });
+
+app.get('/incomelimits', function(req, res){
+  res.render('index', {incomeLimits: incomeLimits});
+});
+
+app.get('/incomelimits/:people', function(req, res){
+  var people = req.params.people;
+  res.render('show', {incomeLimits: incomeLimits[people], people: people})
+})
